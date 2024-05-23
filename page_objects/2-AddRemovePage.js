effc5582096459c20799c11254d07df85caaf159
Lorand FL <florand1998@gmail.com>
2024-05-23 19:31:14 +0300

    Add/Remove page

--- page_objects/2-AddRemovePage.js
+++ page_objects/2-AddRemovePage.js
@@ -5,11 +5,36 @@ export class AddRemoveElemPage extends BasePage {
   constructor(page) {
     super(page);
     this.heading = page.locator("h3");
-    this.addElemButton = page.locator("");
-    this.deleteButton = page.locator("");
+    this.addElemButton = page.locator("button", { hasText: "Add Element" });
+    this.deleteButton = page.locator("button", { hasText: "Delete" });
   }
 
   async isAddRemoveElemPageOpened() {
     await expect(this.page).toHaveURL(/add_remove_elements/);
+  }
+
+  async addElement() {
+    await this.addElemButton.click();
+    await expect(this.deleteButton).toHaveCount(1);
+  }
+
+  async deleteElement() {
+    await this.deleteButton.click();
+    await expect(this.deleteButton).toHaveCount(0);
+  }
+
+  async addMultipleElements(nrOfElements) {
+    for (let i = 0; i < nrOfElements; i++) {
+      await this.addElemButton.click();
+    }
+    await expect(this.deleteButton).toHaveCount(nrOfElements);
+  }
+
+  async checkDeletedElements(nrOfElements) {
+    for (let i = 0; i < nrOfElements; i++) {
+      await expect(this.deleteButton).toHaveCount(nrOfElements);
+      await this.deleteButton.first().click();
+      nrOfElements -= 1;
+    }
   }
 }
--- tests/addRemoveElem-test.spec.js
+++ tests/addRemoveElem-test.spec.js
@@ -4,11 +4,23 @@ import { AddRemoveElemPage } from "../page_objects/2-AddRemovePage";
 
 let basePage, addRemoveElemPage;
 
-test("Add/Remove Element - Test", async ({ page }) => {
-  basePage = new BasePage(page);
-  addRemoveElemPage = new AddRemoveElemPage(page);
+test.describe("Add/Remove Element Page", () => {
+  test.beforeEach(async ({ page }) => {
+    basePage = new BasePage(page);
+    addRemoveElemPage = new AddRemoveElemPage(page);
+    await basePage.loadPage("/");
+    await basePage.navigateToAddRemoveElem();
+    await addRemoveElemPage.isAddRemoveElemPageOpened();
+  });
 
-  await basePage.loadPage("/");
-  await basePage.navigateToAddRemoveElem();
-  await addRemoveElemPage.isAddRemoveElemPageOpened();
+  test("Add/Remove element - Test", async () => {
+    await addRemoveElemPage.addElement();
+    await addRemoveElemPage.deleteElement();
+  });
+
+  test.only("Add/Remove multimple elements", async () => {
+    let numberOfElements = 5;
+    await addRemoveElemPage.addMultipleElements(numberOfElements);
+    await addRemoveElemPage.checkDeletedElements(numberOfElements);
+  });
 });
